b6df08bd03f16705502c27103029b35f639f6c01
mingo <zzmingo@qq.com>
2017-09-06 11:40:07 +0800

    refactor

--- tools/lib/generate.js
+++ tools/lib/generate.js
@@ -2,9 +2,9 @@ const path = require('path')
 const glob = require('glob')
 const xlsx2package = require('./xlsx2package')
 
-const builtins = glob.sync(path.resolve(__dirname, '../xlsx/builtin/*.xlsx'))
+const builtins = glob.sync(path.resolve(__dirname, '../../xlsx/builtin/*.xlsx'))
 builtins.forEach(xlsxFile => {
-  xlsx2package(xlsxFile, path.resolve(__dirname, '../generated/builtin'))
+  xlsx2package(xlsxFile, path.resolve(__dirname, '../../generated/builtin'))
 })
 
 const dlcs = glob.sync(path.resolve(__dirname, '../xlsx/dlc/*.xlsx'))
