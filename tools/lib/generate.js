c84a7feb5722605450baaef6ee422761c1fdbb62
mingo <zzmingo@qq.com>
2017-09-06 19:32:16 +0800

    enscript

--- tools/lib/generate.js
+++ tools/lib/generate.js
@@ -4,10 +4,10 @@ const xlsx2package = require('./xlsx2package')
 
 const builtins = glob.sync(path.resolve(__dirname, '../../xlsx/builtin/*.xlsx'))
 builtins.forEach(xlsxFile => {
-  xlsx2package(xlsxFile, path.resolve(__dirname, '../../generated/builtin'))
+  xlsx2package(xlsxFile, path.resolve(__dirname, '../generated/builtin'))
 })
 
-const dlcs = glob.sync(path.resolve(__dirname, '../xlsx/dlc/*.xlsx'))
+const dlcs = glob.sync(path.resolve(__dirname, '../../xlsx/dlc/*.xlsx'))
 dlcs.forEach(xlsxFile => {
   xlsx2package(xlsxFile, path.resolve(__dirname, '../generated/dlc'))
 })
--- tools/lib/xlsx2package.js
+++ tools/lib/xlsx2package.js
@@ -1,7 +1,10 @@
 const path = require('path')
 const fs = require('fs')
+const crypto = require('crypto')
 const fse = require('fs-extra')
 const XLSX = require('js-xlsx')
+
+const encryptKey = "7ydsdgbp"
 
 function xlsx2package(xlsxFile, dist) {
   const workbook = XLSX.readFile(xlsxFile)
@@ -24,6 +27,7 @@ function xlsx2package(xlsxFile, dist) {
   })
   let dlcCSV = array2csv(dlcArray)
   let dlcPath = path.resolve(dist, dlcId)
+  dlcCSV = encrypt(dlcCSV, encryptKey)
   fse.ensureDirSync(dlcPath)
   fse.writeFileSync(path.resolve(dlcPath, 'Package.csv'), dlcCSV, 'utf8')
 
@@ -37,6 +41,7 @@ function xlsx2package(xlsxFile, dist) {
     return item
   })
   let questionCSV = array2csv(questionArray)
+  questionCSV = encrypt(questionCSV, encryptKey)
   fse.writeFileSync(path.resolve(dlcPath, 'Question.csv'), questionCSV, 'utf8')
 }
 
@@ -51,6 +56,15 @@ function array2csv(array) {
     lines.push(values.join(','))
   })
   return lines.join('\n')
+}
+
+function encrypt (message, key) {
+  key = key.length >= 8 ? key.slice(0, 8) : key.concat('0'.repeat(8 - key.length))
+  const keyHex = new Buffer(key)
+  const cipher = crypto.createCipheriv('des-cbc', keyHex, keyHex)
+  let result = cipher.update(message, 'utf8', 'base64')
+  result += cipher.final('base64')
+  return result
 }
 
 module.exports = xlsx2package
