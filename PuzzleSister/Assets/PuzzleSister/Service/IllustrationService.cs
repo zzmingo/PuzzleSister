580255b8eeb3fda744ffaebde4632a09435dc62c
mingo <zzmingo@qq.com>
2017-09-08 18:56:54 +0800

    updated

--- PuzzleSister/Assets/PuzzleSister/Service/IllustrationService.cs
+++ PuzzleSister/Assets/PuzzleSister/Service/IllustrationService.cs
@@ -6,12 +6,28 @@ using System.IO;
 
 namespace PuzzleSister {
 
+  public class IllustrationItem {
+    public string name;
+    public Sprite image;
+  }
+
   /// <summary>
   /// 图鉴服务
   /// </summary>
   public class IllustrationService {
 
-    
+    public static readonly IllustrationService shared = new IllustrationService();
+
+    private List<IllustrationItem> itemList;
+
+    public void Load() {
+      itemList = new List<IllustrationItem>();
+      
+    }
+
+    public void RetriveItem() {
+
+    }
 
   }
 
