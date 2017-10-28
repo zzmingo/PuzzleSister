02743713df50825e7ae0342ae42af5b095f685a0
zhang.zhengming <zhang.zhengming@uneed.com>
2017-10-28 16:44:21 +0800

    UI更新

--- PuzzleSister/Assets/PuzzleSister/Controller/UIController.cs
+++ PuzzleSister/Assets/PuzzleSister/Controller/UIController.cs
@@ -2,40 +2,67 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UIController : MonoBehaviour {
+namespace PuzzleSister {
+	public class UIController : MonoBehaviour {
 
-	private Stack<GameObject> uiStack = new Stack<GameObject>();
-	public GameObject showingUIObject;
+		[NotNull] public GameObject showingUIObject;
+		[NotNull] public GameObject questionUIObject;
+		[NotNull] public BGMController bGMController;
+		[NotNull] public QuestionController questionController;
 
-	void Start () {
-		foreach(Transform child in transform) {
-			child.gameObject.SetActive(false);
+		private Stack<GameObject> uiStack = new Stack<GameObject>();
+
+		void Awake() {
+			GlobalEvent.shared.AddListener(handleGlobalEvent);
 		}
-		showingUIObject.SetActive(true);
-	}
 
-	public void PushUI(GameObject uiGameObject) {
-		uiGameObject.SetActive(true);
-		showingUIObject.SetActive(false);
-		uiStack.Push(showingUIObject);
-		showingUIObject = uiGameObject;
-	}
-
-	public void PopUI() {
-		if (uiStack.Count <= 0) {
-			return;
+		void handleGlobalEvent(EventData data) {
+			switch(data.type) {
+				case EventType.PackageItemClick:
+					Package package = (data as PackageClickEventData).package;
+          if (package == null) {
+            Utils.ShowDLCStore();
+          } else {
+						PushUI(questionUIObject);
+						bool chanllenge = PackageProgressService.shared.GetProgress(package.id).Completed;
+						questionController.StartPackage(package, chanllenge);
+						bGMController.RandomBGM();
+          }
+					break;
+			}
 		}
-		showingUIObject.SetActive(false);
-		showingUIObject = uiStack.Pop();
-		showingUIObject.SetActive(true);
-	}
 
-	void Update() {
-#if UNITY_STANDALONE
-      if (Input.GetKeyUp(KeyCode.Escape)) {
-        PopUI();
-      }
-#endif
-    }
+		void Start () {
+			foreach(Transform child in transform) {
+				child.gameObject.SetActive(false);
+			}
+			showingUIObject.SetActive(true);
+		}
+
+		public void PushUI(GameObject uiGameObject) {
+			uiGameObject.SetActive(true);
+			showingUIObject.SetActive(false);
+			uiStack.Push(showingUIObject);
+			showingUIObject = uiGameObject;
+		}
+
+		public void PopUI() {
+			if (uiStack.Count <= 0) {
+				return;
+			}
+			showingUIObject.SetActive(false);
+			showingUIObject = uiStack.Pop();
+			showingUIObject.SetActive(true);
+		}
+
+		void Update() {
+	#if UNITY_STANDALONE
+				if (Input.GetKeyUp(KeyCode.Escape)) {
+					PopUI();
+				}
+	#endif
+			}
+
+	}
 
 }
