0c2ddbd2cfcc6df9a7de6102307bef67d3e81b10
aaronrzh <287628769@qq.com>
2018-03-16 18:07:56 +0800

    修改设置

---
+++ PuzzleSister/Assets/PuzzleSister/Settings/SettingManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SettingManager : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IMoveHandler {
+	
+	private static SettingManager instance;
+	public static SettingManager Instance {
+		get {
+			if (instance == null) {
+				return new GameObject("SettingManager").AddComponent<SettingManager>();
+			} else {
+				return instance;
+			}
+		}
+	}
+
+	private 
+
+    void Start () {
+		if (instance != null) {
+			Destroy(gameObject);
+			return;
+		}
+
+		instance = this;
+
+		DontDestroyOnLoad(gameObject);
+	}
+	
+	void Update () {
+		
+	}
+
+	void OnDestroy() {
+		if (instance != this) {
+			return;
+		}
+
+		instance = null;
+	}
+
+    public void OnMove(AxisEventData eventData) {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPointerDown(PointerEventData eventData) {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPointerUp(PointerEventData eventData) {
+        throw new System.NotImplementedException();
+    }
+}
