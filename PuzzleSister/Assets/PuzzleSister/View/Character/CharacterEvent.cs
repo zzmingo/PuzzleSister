b7035891fff7694e6064ee79c1c94a6c395c8c51
aaronrzh <287628769@qq.com>
2018-05-18 17:13:28 +0800

    修改眨眼事件

--- PuzzleSister/Assets/PuzzleSister/View/Character/CharacterEvent.cs
+++ PuzzleSister/Assets/PuzzleSister/View/Character/CharacterEvent.cs
@@ -9,23 +9,18 @@ namespace PuzzleSister {
 
 		void OnEnable() {
 			if (!animator) {
-				animator = this.GetComponent<Animator>();
+				animator = GetComponent<Animator>();
 			}
-			animator.Play("Idle");
-			animator.Update(0);
-			animator.SetBool("Blink", false);
 			StartCoroutine(wait(Random.Range(3, 10)));
 		}
 
 		void EyesNormal() {
-			animator.SetBool("Blink", false);
-			animator.Play("Idle");
-			animator.Update(0);
 			StartCoroutine(wait(Random.Range(3, 10)));
 		}
 
 		IEnumerator wait(float s) {
-			yield return new WaitForSeconds (s);
+			animator.SetBool("Blink", false);
+			yield return new WaitForSeconds(s);
 			animator.SetBool("Blink", true);
 		}
 	}
