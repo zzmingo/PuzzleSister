71fd7a2cc616590ca8aaa5a7255a69345470363f
aaronrzh <287628769@qq.com>
2018-06-08 10:28:58 +0800

    修改全屏切换问题

--- PuzzleSister/Assets/PuzzleSister/UGCEditor/ImageSelector.cs
+++ PuzzleSister/Assets/PuzzleSister/UGCEditor/ImageSelector.cs
@@ -7,49 +7,59 @@ using System;
 using SFB;
 using UnityEngine.Events;
 
-namespace PuzzleSister.UGCEditor {
+namespace PuzzleSister.UGCEditor
+{
 
-  public class ImageSelector : MonoBehaviour {
+    public class ImageSelector : MonoBehaviour
+    {
 
-    public UnityEvent OnSelected;
-    [NotNull] public Image image;
+        public UnityEvent OnSelected;
+        [NotNull] public Image image;
 
-    public String imagePath;
+        public String imagePath;
 
-    public void UpdateImage() {
-      if (!String.IsNullOrEmpty(imagePath)) {
-        StartCoroutine(UpdateImageAsync());
-      } else {
-        image.sprite = null;
-      }
+        public void UpdateImage()
+        {
+            if (!String.IsNullOrEmpty(imagePath))
+            {
+                StartCoroutine(UpdateImageAsync());
+            }
+            else
+            {
+                image.sprite = null;
+            }
+        }
+
+        public void OpenSelector()
+        {
+            var extensions = new[] { new ExtensionFilter("Image Files", "png", "jpg", "jpeg" ) };
+            var paths = StandaloneFileBrowser.OpenFilePanel("Open File", "", extensions, false);
+            if (paths.Length > 0 && !string.IsNullOrEmpty(paths[0]))
+            {
+                var sprite = SpriteExtensions.FromFile(paths[0]);
+                image.sprite = sprite;
+                imagePath = paths[0];
+                OnSelected.Invoke();
+            }
+        }
+
+        IEnumerator UpdateImageAsync()
+        {
+            WWW www = new WWW(imagePath);
+            yield return www;
+            if (www.texture != null)
+            {
+                image.sprite = Sprite.Create(
+                  www.texture,
+                  new Rect(0, 0, www.texture.width, www.texture.height),
+                  new Vector2(0.5f, 0.5f));
+            }
+            else
+            {
+                image.sprite = null;
+            }
+        }
+
     }
-
-    public void OpenSelector() {
-      var extensions = new [] {
-        new ExtensionFilter("Image Files", "png", "jpg", "jpeg" )
-      };
-      var paths = StandaloneFileBrowser.OpenFilePanel("Open File", "", extensions, false);
-      if (paths.Length > 0 && !string.IsNullOrEmpty(paths[0])) {
-        var sprite = SpriteExtensions.FromFile(paths[0]);
-        image.sprite = sprite;
-        imagePath = paths[0];
-        OnSelected.Invoke();
-      }
-    }
-
-    IEnumerator UpdateImageAsync() {
-      WWW www = new WWW(imagePath);
-      yield return www;
-      if (www.texture != null) {
-        image.sprite = Sprite.Create(
-          www.texture, 
-          new Rect(0, 0, www.texture.width, www.texture.height), 
-          new Vector2(0.5f, 0.5f));
-      } else {
-        image.sprite = null;
-      }
-    }
-
-  }
 
 }
--- PuzzleSister/Assets/PuzzleSister/UGCEditor/PackageItem.cs
+++ PuzzleSister/Assets/PuzzleSister/UGCEditor/PackageItem.cs
@@ -8,45 +8,49 @@ using UnityEngine.UI;
 using Steamworks;
 #endif
 
-namespace PuzzleSister.UGCEditor {
+namespace PuzzleSister.UGCEditor
+{
 
-  public class PackageItem {
+    public class PackageItem
+    {
 
-    public String id;
-    public String name;
-		public String author;
-		public String language;
-    public String description;
-    public String imagePath;
-    public uint timeUpdated;
-    public bool visible;
-    public PublishedFileId_t publishedFileId = PublishedFileId_t.Invalid;
-    public int questionCount;
+        public String id;
+        public String name;
+        public String author;
+        public String language;
+        public String description;
+        public String imagePath;
+        public uint timeUpdated;
+        public bool visible;
+        public PublishedFileId_t publishedFileId = PublishedFileId_t.Invalid;
+        public int questionCount;
 
-    public void Set(PackageItem other) {
-      id = other.id;
-      name = other.name;
-			author = other.author;
-			language = other.language;
-      description = other.description;
-      imagePath = other.imagePath;
-      publishedFileId = other.publishedFileId;
-      questionCount = other.questionCount;
+        public void Set(PackageItem other)
+        {
+            id = other.id;
+            name = other.name;
+            author = other.author;
+            language = other.language;
+            description = other.description;
+            imagePath = other.imagePath;
+            publishedFileId = other.publishedFileId;
+            questionCount = other.questionCount;
+        }
+
+        public PackageItem Clone()
+        {
+            PackageItem pkg = new PackageItem();
+            pkg.id = id;
+            pkg.name = name;
+            pkg.author = author;
+            pkg.language = language;
+            pkg.description = description;
+            pkg.imagePath = imagePath;
+            pkg.publishedFileId = publishedFileId;
+            pkg.questionCount = questionCount;
+            return pkg;
+        }
+
     }
-
-    public PackageItem Clone() {
-      PackageItem pkg = new PackageItem();
-      pkg.id = id;
-      pkg.name = name;
-			pkg.author = author;
-			pkg.language = language;
-      pkg.description = description;
-      pkg.imagePath = imagePath;
-      pkg.publishedFileId = publishedFileId;
-      pkg.questionCount = questionCount;
-      return pkg;
-    }
-
-  }
 
 }
